2548724173e4da3a9fa74143d2b1361c746d49a8
Victor A <VictorOluwaseun@Macs-MacBook-Air.local>
2020-08-26 08:53:58 +0100

    Updated

--- app/src/androidTest/java/com/decagon/useractivity/SharedPrefManagerTest.kt
+++ app/src/androidTest/java/com/decagon/useractivity/SharedPrefManagerTest.kt
@@ -2,7 +2,6 @@ package com.decagon.useractivity
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Assert.assertEquals
 import org.hamcrest.core.Is.`is`
 import org.junit.Before
 import org.junit.Test
@@ -11,31 +10,16 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SharedPrefManagerTest {
-//    @Test
-//    fun test_clearData(){
-////        val sharedPrefs: HashMap<String, String>
-//        val sharedPrefs : HashMap<String, String> = HashMap<String, String>()
-//
-//        sharedPrefs.put("state", "Niger")
-//        sharedPrefs.put("city", "Minna")
-//
-//
-////        val sharedPrefManagerObj = SharedPrefManager(hashMapOf())
-//
-//        assertEquals(SharedPrefManager(sharedPrefs).clearData(), `is`(sharedPrefs.clear()))
-//    }
-
-
 
     var sharedPrefs = HashMap<String, String>()
     private var sharedObj = SharedPrefManager(sharedPrefs)
-    private val key = "3"
-    private val user = "Joi"
+    private val state = "Niger"
+    private val city = "Minna"
 
     @Before
     fun ini () {
-        sharedPrefs["1"] = "User 1"
-        sharedPrefs["2"] = "User 2"
+        sharedPrefs["state"] = "Lagos"
+        sharedPrefs["city"] = "Ikeja"
     }
 
     @Test
@@ -79,17 +63,17 @@ class SharedPrefManagerTest {
     @Test
     fun saveData_data_returnArrayList() {
 
-        val userJson = sharedObj.gson.toJson(user)
+        val userJson = sharedObj.gson.toJson(city)
         var result = arrayListOf(userJson)
-        sharedPrefs[key] = userJson
+        sharedPrefs[state] = userJson
 
-        assertThat(result, `is`(sharedObj.saveData(user,key)))
+        assertThat(result, `is`(sharedObj.saveData(city,state)))
     }
 
     @Test
     fun keepData_user_storeUser() {
-        val userJsn = sharedObj.gson.toJson(user)
-        sharedPrefs[key] = userJsn
+        val userJsn = sharedObj.gson.toJson(city)
+        sharedPrefs[state] = userJsn
 
     }
 
@@ -98,7 +82,7 @@ class SharedPrefManagerTest {
         val user = ""
         val getUser = sharedObj.gson.fromJson(user, User::class.java)
 
-        assertThat(getUser, `is`(sharedObj.checkUser(key)))
+        assertThat(getUser, `is`(sharedObj.checkUser(state)))
     }
 
 }
